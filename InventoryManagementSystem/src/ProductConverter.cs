2d261075855fe073c3f3068236cf3ef6734e2e7e
zgraceman <zach.graceman@gmail.com>
2024-05-01 17:42:14 -0500

    Created new ProductConverter class for custom JSON conversion for deserialization

---
+++ InventoryManagementSystem/src/ProductConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace InventoryManagementSystem.src
+{
+    public class ProductConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(Product));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject item = JObject.Load(reader);
+            switch (item["Type"].ToString())
+            {
+                case "Book":
+                    return item.ToObject<Book>(serializer);
+                case "Device":
+                    return item.ToObject<Device>(serializer);
+                default:
+                    throw new JsonSerializationException("Unknown type");
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException(); // Not needed if you only deserialize
+        }
+    }
+}
+

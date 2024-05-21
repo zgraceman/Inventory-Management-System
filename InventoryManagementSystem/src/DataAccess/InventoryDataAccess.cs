dab5ea3cc727eb8d9dcdad17e97a1e3b9df629e3
zgraceman <zach.graceman@gmail.com>
2024-05-20 19:29:17 -0500

    Move and refractor data persistence related methods to new InventoryDataAccess class

---
+++ InventoryManagementSystem/src/DataAccess/InventoryDataAccess.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace InventoryManagementSystem.src.DataAccess
+{
+	public class InventoryDataAccess
+	{
+		public InventoryDataAccess()
+		{
+		}
+
+
+        /// <summary>
+        /// Saves the current state of the inventory to a JSON file.
+        /// </summary>
+        /// <param name="filePath">The file path where the JSON file will be saved.</param>
+        public void SaveToJson(string filePath, List<Product> products)
+        {
+            var jsonString = JsonConvert.SerializeObject(products, Formatting.Indented);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        /// <summary>
+        /// Loads the inventory from a JSON file.
+        /// </summary>
+        /// <param name="filePath">The file path of the JSON file to be loaded.</param>
+        public List<Product> LoadFromJson(string filePath)
+        {
+            var jsonString = File.ReadAllText(filePath);
+            // Create JsonSerializerSettings and use custom converter
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            {
+                Converters = new List<JsonConverter> { new ProductConverter() }
+            };
+            // Deserialize the JSON string using the specified settings
+            return JsonConvert.DeserializeObject<List<Product>>(jsonString, settings);
+        }
+
+        /// <summary>
+        /// Saves the current state of the inventory to an XML file.
+        /// </summary>
+        /// <param name="filePath">The file path where the XML file will be saved.</param>
+        public void SaveToXml(string filePath, List<Product> products)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Product>));
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, products);
+            }
+        }
+
+        /// <summary>
+        /// Loads the inventory from an XML file.
+        /// </summary>
+        /// <param name="filePath">The file path of the XML file to be loaded.</param>
+        public List<Product> LoadFromXml(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                // Create a new XmlSerializer instance with the type of the List<Product>
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Product>));
+
+                // Open the file specified by filePath using a FileStream
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    // Deserialize the XML from the file to the products list
+                    return (List<Product>)serializer.Deserialize(fileStream);
+                }
+            }
+            return new List<Product>();
+        }
+
+    }
+}
+
